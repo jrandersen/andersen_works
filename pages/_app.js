3053ddc3d9d3fea2a02c2eb458d09e0ded5fbc19
jandersen <andersen.ja@gmail.com>
2022-04-18 15:51:18 -0400

    adds python, csharp and js code highlighter

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
 import 'tailwindcss/tailwind.css'
+import Prism from 'prism-react-renderer/prism'
+
+(typeof global !== "undefined" ? global : window).Prism = Prism
+
+require("prismjs/components/prism-python")
+require("prismjs/components/prism-csharp")
+require("prismjs/components/prism-javascript")
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
