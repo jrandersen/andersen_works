703ff054c443b8248326b5ba767ce8a162877885
jandersen <andersen.ja@gmail.com>
2022-04-18 15:07:10 -0400

    update header

--- components/header.js
+++ components/header.js
@@ -4,14 +4,9 @@ export function Header() {
     <div className="flex items-center justify-between max-w-3xl px-8 mx-auto py-4">
       <Link href="/">
         <a className="block font-bold text-lg">Jason Andersen</a>
-
       </Link>
       <a target="_blank" href="https://twitter.com/jasonrandersen" rel="noopener">Follow Me</a>
-      <span className="flex items-center justify-between max-w-3xl px-4 mx-auto py-4">
-      &#10;this text should be on a new line. This is some new text,
-      </span>
+      <a className="block font-bold text-lg"> &#10;this text should be on a new line. This is some new text</a>
     </div>
-
-
   );
 }
