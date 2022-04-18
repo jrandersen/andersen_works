1745e2996aa17a0e146590d728f4d42705da72f0
jandersen <andersen.ja@gmail.com>
2022-04-18 14:11:08 -0400

    update header

--- components/header.js
+++ components/header.js
@@ -4,10 +4,10 @@ export function Header() {
     <div className="flex items-center justify-between max-w-3xl px-8 mx-auto py-4">
       <Link href="/">
         <a className="block font-bold text-lg">Jason Andersen</a>
+
       </Link>
-      <a target="_blank" href="https://twitter.com/jasonrandersen" rel="noopener">
-        Follow Me
-      </a>
+      <a target="_blank" href="https://twitter.com/jasonrandersen" rel="noopener">Follow Me</a>
+      <a className="text-lg">This is now some textn</a>
     </div>
   );
 }
