98b09c4e7e52aaee7e73e1ff2fe554be8f21a5eb
jandersen <andersen.ja@gmail.com>
2022-04-18 21:05:46 -0400

    updated tailwind css back to original

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,42 +3,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      grey: {
-        100: "#F5F7FA",
-        1000: "#1F2933"
-      },
-    },
-    typography: theme => ({
-      default: {
-        css: {
-          pre: {
-            color: theme("colors.grey.1000"),
-            backgroundColor: theme("colors.grey.100")
-          },
-          "pre code::before": {
-            "padding-left": "unset"
-          },
-          "pre code::after": {
-            "padding-right": "unset"
-          },
-          code: {
-            backgroundColor: theme("colors.grey.100"),
-            color: "#DD1144",
-            fontWeight: "400",
-            "border-radius": "0.25rem"
-          },
-          "code::before": {
-            content: '""',
-            "padding-left": "0.25rem"
-          },
-          "code::after": {
-            content: '""',
-            "padding-right": "0.25rem"
-          }
-        }
-      }
-    })
+    extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
 };
