1774b89588a8e38b3cf17377e2e38306b7f54062
jandersen <andersen.ja@gmail.com>
2022-04-20 17:18:49 -0400

    add favicon

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home(props) {
   return (
     <>
       <Header />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
       <div className="max-w-3xl px-8 mx-auto py-4">
         <ul className="space-y-3">
           {posts.posts.map((p) => (
