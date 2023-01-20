379e52f7de0282a14d76c51824e720e38a884284
jandersen <andersen.ja@gmail.com>
2023-01-20 10:21:32 -0500

    updates title

--- components/header.js
+++ components/header.js
@@ -1,11 +1,11 @@
 import Link from 'next/link';
-export function Header({ title = 'Andersen' }) {
+export function Header({ title = '' }) {
   return (
     <div className="flex items-center justify-between max-w-3xl px-8 mx-auto py-4">
       <Link href="/">
         <a className="block font-bold text-lg">Jason Andersen</a>
       </Link>
-      <a target="_blank" href="https://twitter.com/jasonrandersen" rel="noopener">Follow Me</a>
+      <a target="_blank" href="https://twitter.com/jasonrandersen" rel="noopener">Follow</a>
     </div>
   );
 }
