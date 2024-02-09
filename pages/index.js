f10f01b0ff8bd84bfae06727c551c07ead0986b8
jandersen <andersen.ja@gmail.com>
2024-02-09 11:27:54 -0500

    updated header

--- pages/index.js
+++ pages/index.js
@@ -6,20 +6,20 @@ export default function Home(props) {
   return (
     <>
       <Header />
-      <div className="max-w-3xl px-8 mx-auto py-4">
-        <ul className="space-y-3">
-          {posts.posts.map((p) => (
-            <li key={p.slug} className="flex items-left space-x-3">
-              <span className="block text-gray-500 w-[9.5rem] text-right">
-                {p.date}
-              </span>
-              <Link href={p.slug}>
-                <a className="block font-bold">{p.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+        <div className="max-w-3xl px-8 mx-auto py-4">
+          <ul className="space-y-3">
+            {posts.posts.map((p) => (
+              <li key={p.slug} className="flex items-left space-x-3">
+                <span className="block text-gray-500 w-[9.5rem] text-right">
+                  {p.date}
+                </span>
+                <Link href={p.slug}>
+                  <a className="block font-bold">{p.title}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
     </>
   );
 }
